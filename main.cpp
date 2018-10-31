edce78800ef9d81708321baa732528f80e7a50df
Rudolph Lorenz <rudlorenz@gmail.com>
2018-10-31 23:56:03 +0700

    Move source files to src

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "graph.h"
+#include "src/graph.h"
 
 #include <iostream>
 
@@ -19,22 +19,22 @@ int main()
 
     std::cout << glp << "\n\n";
 
-    glp.writeasJSON("grph.json");
+    glp.writeasJSON("graph.json");
 
-    std::cout << std::boolalpha;
-
-    std::cout << glp.getptr() << " --> 2 : " << glp.traverese(2) << "\n";
-    std::cout << glp.getptr() << " --> 4 : " << glp.traverese(4) << "\n";
-    std::cout << glp.getptr() << " --> 5 : " << glp.traverese(5) << "\n";
-    std::cout << glp.getptr() << " --> 3 : " << glp.traverese(3) << "\n";
-    std::cout << glp.getptr() << " --> 8 : " << glp.traverese(8) << "\n";
-    std::cout << "result : " << glp.getptr() << "\n\n";
-
-    for (int i = 0; i < 10; i++)
-    {
-        if (i == 3) { continue; }
-        std::cout << glp.getptr() << " --> " << i <<  " : " << glp.traverese(i) << "\n";
-    }
+//    std::cout << std::boolalpha;
+//
+//    std::cout << glp.getptr() << " --> 2 : " << glp.traverese(2) << "\n";
+//    std::cout << glp.getptr() << " --> 4 : " << glp.traverese(4) << "\n";
+//    std::cout << glp.getptr() << " --> 5 : " << glp.traverese(5) << "\n";
+//    std::cout << glp.getptr() << " --> 3 : " << glp.traverese(3) << "\n";
+//    std::cout << glp.getptr() << " --> 8 : " << glp.traverese(8) << "\n";
+//    std::cout << "result : " << glp.getptr() << "\n\n";
+//
+//    for (int i = 0; i < 10; i++)
+//    {
+//        if (i == 3) { continue; }
+//        std::cout << glp.getptr() << " --> " << i <<  " : " << glp.traverese(i) << "\n";
+//    }
 
     return 0;
 }
